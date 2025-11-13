105aebe9e32513cf63acb878094bf39d93913686
Samuel Aguiar <72396666+aguiarsc@users.noreply.github.com>
2025-11-13 13:48:46 +0100

    fix(motion): upgrade to newest motion dependency
    -200KB removing dupe obsolete package

--- components/site-nav.tsx
+++ components/site-nav.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
-import { motion } from 'framer-motion'
+import { motion } from 'motion/react'
 import { AnimatedThemeToggler } from '@/components/animated-theme-toggler'
 import { SearchCommand } from '@/components/search-command'
 
